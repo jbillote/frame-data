f2241e93fb4e336d60a72db6cb64b02830285b06
jbillote <jbillote@gmail.com>
2023-11-30 15:46:15 -0800

    Basic GET functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    extract:: {Path},
+    routing::{get},
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/character/:character_name", get(character_info));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn character_info(Path(character_name): Path<String>) -> String {
+    return character_name;
 }
