afc4dd8e73b32939c461c86bf715b4eb4ec2d8c1
jbillote <jbillote@gmail.com>
2023-12-23 17:55:40 -0800

    Set up deployment on Shuttle

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,16 @@
 use crate::controllers::attack::{get_attack, get_attacks};
 use crate::controllers::character::get_characters;
-use axum::{extract::OriginalUri, http::Request, routing::get, Router};
-use tower_http::trace::TraceLayer;
-use tracing::info_span;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use axum::{routing::get, Router};
 
 mod controllers;
 mod models;
 
-#[tokio::main]
-async fn main() {
-    tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "frame_data=info,tower_http=info,axum::rejection=trace".into()),
-        )
-        .with(tracing_subscriber::fmt::layer())
-        .init();
-
+#[shuttle_runtime::main]
+async fn axum() -> shuttle_axum::ShuttleAxum {
     let app = Router::new()
         .route("/character/:character_name", get(get_attacks))
         .route("/character/:character_name/:input", get(get_attack))
-        .route("/characters", get(get_characters))
-        .layer(
-            TraceLayer::new_for_http().make_span_with(|request: &Request<_>| {
-                let path = if let Some(path) = request.extensions().get::<OriginalUri>() {
-                    path.0.path().to_owned()
-                } else {
-                    request.uri().path().to_owned()
-                };
+        .route("/characters", get(get_characters));
 
-                info_span!(
-                    "http_request",
-                    method = ?request.method(),
-                    path,
-                )
-            }),
-        );
-
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
-    tracing::debug!("Listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app).await.unwrap();
+    Ok(app.into())
 }
